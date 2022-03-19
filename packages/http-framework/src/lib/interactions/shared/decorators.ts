3f66b188deafcfa184389f4f86907ece6ebc0c5c
Jeroen Claassens <support@favware.tech>
2022-03-19 20:26:31 +0100

    fix(framework): allow more types for `RestrictGuildIds`

--- packages/http-framework/src/lib/interactions/shared/decorators.ts
+++ packages/http-framework/src/lib/interactions/shared/decorators.ts
@@ -1,9 +1,9 @@
 import { Collection } from '@discordjs/collection';
 import type { Command } from '../../structures/Command';
 
-export const restrictedGuildIdRegistry = new Collection<typeof Command, readonly [string, ...string[]]>();
+export const restrictedGuildIdRegistry = new Collection<typeof Command, string[]>();
 
-export function RestrictGuildIds(guildIds: readonly [string, ...string[]]) {
+export function RestrictGuildIds(guildIds: string[]) {
 	return function decorate(target: typeof Command) {
 		restrictedGuildIdRegistry.set(target, guildIds);
 	};
