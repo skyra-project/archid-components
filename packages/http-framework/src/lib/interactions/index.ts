bc7f8f608950f3e52ea201d6056683098aef3569
Jeroen Claassens <support@favware.tech>
2022-04-02 20:05:01 +0200

    refactor: remove argumentsof

--- packages/http-framework/src/lib/interactions/index.ts
+++ packages/http-framework/src/lib/interactions/index.ts
@@ -3,6 +3,5 @@ export * from './chat-input/shared';
 export * from './context-menu/decorators';
 export * from './context-menu/shared';
 export * from './context-menu/util';
-export * from './resolvers/ArgumentsOf';
 export * from './resolvers/InteractionOptions';
 export * from './shared/decorators';
