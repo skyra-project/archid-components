338e3644ae1d5b8d982e4a95b69d6dd1cdde447b
Jeroen Claassens <support@favware.tech>
2022-04-12 23:22:33 +0200

    chore: skip git hooks when doing releases

--- scripts/post-tag.ps1
+++ scripts/post-tag.ps1
@@ -1,6 +1,6 @@
 yarn changelog
 git add CHANGELOG.md
-git commit --amend --no-edit
+git commit --amend --no-edit --no-verify
 
 $latestTag = $(git describe --tags $(git rev-list --tags --max-count=1))
 $latestCommit = $(git rev-list HEAD | head -1)
