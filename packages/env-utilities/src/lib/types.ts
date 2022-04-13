d7dc62d4b5db7b1ff71ce5ab09ebf02105ceaf2e
Antonio Román <kyradiscord@gmail.com>
2022-04-14 00:56:40 +0200

    fix(env-utilities): resolved some typing issues

--- packages/env-utilities/src/lib/types.ts
+++ packages/env-utilities/src/lib/types.ts
@@ -1,12 +1,14 @@
 export type BooleanString = 'true' | 'false';
 export type IntegerString = `${bigint}`;
 export type NumberString = `${number}`;
+export type ArrayString = string & { __type__: 'ArrayString' };
 
 export type EnvAny = keyof Env;
 export type EnvString = { [K in EnvAny]: Env[K] extends BooleanString | IntegerString | NumberString ? never : K }[EnvAny];
 export type EnvBoolean = { [K in EnvAny]: Env[K] extends BooleanString | undefined ? K : never }[EnvAny];
 export type EnvInteger = { [K in EnvAny]: Env[K] extends IntegerString | undefined ? K : never }[EnvAny];
 export type EnvNumber = { [K in EnvAny]: Env[K] extends NumberString | undefined ? K : never }[EnvAny];
+export type EnvArray = { [K in EnvAny]: Env[K] extends ArrayString | undefined ? K : never }[EnvAny];
 
 export interface Env {
 	NODE_ENV: 'test' | 'development' | 'production';
--- packages/env-utilities/src/lib/utils.ts
+++ packages/env-utilities/src/lib/utils.ts
@@ -1,7 +1,7 @@
-import type { Env, EnvAny, EnvBoolean, EnvInteger, EnvNumber, EnvString } from './types';
+import type { Env, EnvAny, EnvArray, EnvBoolean, EnvInteger, EnvNumber, EnvString } from './types';
 
-export function envParseInteger(key: EnvInteger, defaultValue: number | null): number | null;
 export function envParseInteger(key: EnvInteger, defaultValue?: number): number;
+export function envParseInteger(key: EnvInteger, defaultValue: number | null): number | null;
 export function envParseInteger(key: EnvInteger, defaultValue?: number | null): number | null {
 	const value = process.env[key];
 	if (!value) {
@@ -14,8 +14,8 @@ export function envParseInteger(key: EnvInteger, defaultValue?: number | null): 
 	throw new TypeError(`[ENV] ${key} - The key must be an integer, but received '${value}'.`);
 }
 
-export function envParseNumber(key: EnvNumber, defaultValue: number | null): number | null;
 export function envParseNumber(key: EnvNumber, defaultValue?: number): number;
+export function envParseNumber(key: EnvNumber, defaultValue: number | null): number | null;
 export function envParseNumber(key: EnvNumber, defaultValue?: number | null): number | null {
 	const value = process.env[key];
 	if (!value) {
@@ -28,8 +28,8 @@ export function envParseNumber(key: EnvNumber, defaultValue?: number | null): nu
 	throw new TypeError(`[ENV] ${key} - The key must be a number, but received '${value}'.`);
 }
 
-export function envParseBoolean(key: EnvBoolean, defaultValue: boolean | null): boolean | null;
 export function envParseBoolean(key: EnvBoolean, defaultValue?: boolean): boolean;
+export function envParseBoolean(key: EnvBoolean, defaultValue: boolean | null): boolean | null;
 export function envParseBoolean(key: EnvBoolean, defaultValue?: boolean | null): boolean | null {
 	const value = process.env[key];
 	if (!value) {
@@ -42,21 +42,21 @@ export function envParseBoolean(key: EnvBoolean, defaultValue?: boolean | null):
 	throw new TypeError(`[ENV] ${key} - The key must be a boolean, but received '${value}'.`);
 }
 
-export function envParseString<K extends EnvString>(key: K, defaultValue: Env[K] | null): Env[K] | null;
-export function envParseString<K extends EnvString>(key: K, defaultValue?: Env[K]): Env[K];
-export function envParseString<K extends EnvString>(key: K, defaultValue?: Env[K] | null): Env[K] | null {
+export function envParseString<K extends EnvString>(key: K, defaultValue?: Env[K]): NonNullable<Env[K]>;
+export function envParseString<K extends EnvString>(key: K, defaultValue: Env[K] | null): NonNullable<Env[K]> | null;
+export function envParseString<K extends EnvString>(key: K, defaultValue?: Env[K] | null): NonNullable<Env[K]> | null {
 	const value = process.env[key];
 	if (!value) {
 		if (defaultValue === undefined) throw new ReferenceError(`[ENV] ${key} - The key must be a string, but is empty or undefined.`);
-		return defaultValue;
+		return defaultValue!;
 	}
 
-	return value;
+	return value!;
 }
 
-export function envParseArray(key: EnvString, defaultValue: string[] | null): string[] | null;
-export function envParseArray(key: EnvString, defaultValue?: string[]): string[];
-export function envParseArray(key: EnvString, defaultValue?: string[] | null): string[] | null {
+export function envParseArray(key: EnvArray, defaultValue?: string[]): string[];
+export function envParseArray(key: EnvArray, defaultValue: string[] | null): string[] | null;
+export function envParseArray(key: EnvArray, defaultValue?: string[] | null): string[] | null {
 	const value = process.env[key];
 	if (!value) {
 		if (defaultValue === undefined) throw new ReferenceError(`[ENV] ${key} - The key must be an array, but is empty or undefined.`);
