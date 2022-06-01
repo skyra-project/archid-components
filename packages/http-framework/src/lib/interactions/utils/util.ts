3a83f6917ebc36a7bbcc6c2963a5494e2af2cc16
A. Román <kyradiscord@gmail.com>
2022-06-01 22:41:14 +0200

    fix(hfx): handle errors on first `generator.next()` (#82)

--- packages/http-framework/src/lib/interactions/utils/util.ts
+++ packages/http-framework/src/lib/interactions/utils/util.ts
@@ -140,7 +140,12 @@ async function handleGenerator(
 	interaction: NonPingInteraction,
 	generator: InteractionHandlerGeneratorResponse
 ): Promise<FastifyReply> {
-	let result = await generator.next();
+	let result;
+	try {
+		result = await generator.next();
+	} catch (error) {
+		return handleError(reply, error);
+	}
 
 	// If the method was a generator...
 	if (result.done) {
