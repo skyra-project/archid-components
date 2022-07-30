c0a7542e21b9deeb4939bdf75b8c8a66f6edc061
Jeroen Claassens <support@favware.tech>
2022-07-30 22:35:02 +0200

    feat(http-framework-i18n): add builder helper methods

--- packages/http-framework-i18n/src/lib/utils.ts
+++ packages/http-framework-i18n/src/lib/utils.ts
@@ -1,8 +1,10 @@
+import { Collection } from '@discordjs/collection';
 import type { NonNullObject } from '@sapphire/utilities';
-import type { APIInteraction, APIPingInteraction, LocaleString } from 'discord-api-types/v10';
+import { lazy } from '@sapphire/utilities';
+import type { APIInteraction, APIPingInteraction, LocaleString, LocalizationMap } from 'discord-api-types/v10';
 import type { TFunction, TOptions, TOptionsBase } from 'i18next';
 import { getT, loadedLocales } from './registry';
-import type { TypedFT, TypedT } from './types';
+import type { BuilderWithNameAndDescription, LocalePrefixKey, TypedFT, TypedT } from './types';
 
 export type Interaction = Exclude<APIInteraction, APIPingInteraction>;
 
@@ -66,4 +68,46 @@ export function resolveKey<TArgs extends NonNullObject, TReturn>(
 ): TReturn;
 export function resolveKey(interaction: Interaction, ...args: [any, any, any?]) {
 	return getSupportedLanguageT(interaction)(...args);
+}
+
+const getLocales = lazy(() => new Collection([...loadedLocales].map((locale) => [locale, getT(locale)])));
+
+function getDefaultT() {
+	const defaultT = getLocales().get('en-US');
+	if (defaultT) return defaultT;
+	throw new TypeError('Could not find en-US');
+}
+
+export function applyLocalizedBuilder<T extends BuilderWithNameAndDescription>(
+	builder: T,
+	...params: [root: LocalePrefixKey] | [name: TypedT, description: TypedT]
+): T {
+	const locales = getLocales();
+	const defaultT = getDefaultT();
+
+	const [localeName, localeDescription] = params.length === 1 ? [`${params[0]}Name` as TypedT, `${params[0]}Description` as TypedT] : params;
+
+	return builder
+		.setName(defaultT(localeName))
+		.setNameLocalizations(Object.fromEntries(locales.map((t, locale) => [locale, t(localeName)])))
+		.setDescription(defaultT(localeDescription))
+		.setDescriptionLocalizations(Object.fromEntries(locales.map((t, locale) => [locale, t(localeDescription)])));
+}
+
+export function createSelectMenuChoiceName<V extends NonNullObject>(nameKey: TypedT, value?: V): createSelectMenuChoiceName.Result<V> {
+	const locales = getLocales();
+	const defaultT = getDefaultT();
+
+	return {
+		...value,
+		name: defaultT(nameKey),
+		name_localizations: Object.fromEntries(locales.map((t, locale) => [locale, t(nameKey)]))
+	} as createSelectMenuChoiceName.Result<V>;
+}
+
+export namespace createSelectMenuChoiceName {
+	export type Result<V> = V & {
+		name: string;
+		name_localizations: LocalizationMap;
+	};
 }
