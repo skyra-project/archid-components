d494df72bde27cad72ae843db65238d65f331ae4
Antonio Román <kyradiscord@gmail.com>
2022-08-07 10:36:36 +0200

    feat: add `applyNameLocalizedBuilder` among others

--- packages/http-framework-i18n/src/lib/types.ts
+++ packages/http-framework-i18n/src/lib/types.ts
@@ -1,5 +1,4 @@
 import type { NonNullObject } from '@sapphire/utilities';
-import type { LocalizationMap } from 'discord-api-types/v10';
 
 export type TypedT<TCustom = string> = string & { __type__: TCustom };
 
@@ -25,13 +24,6 @@ export interface Values<T = string> {
 export interface Difference<T = string> {
 	previous: T;
 	next: T;
-}
-
-export interface BuilderWithNameAndDescription {
-	setName(name: string): this;
-	setNameLocalizations(localizedNames: LocalizationMap | null): this;
-	setDescription(description: string): this;
-	setDescriptionLocalizations(localizedDescriptions: LocalizationMap | null): this;
 }
 
 export type LocalePrefixKey = `commands/${string}:${string}`;
