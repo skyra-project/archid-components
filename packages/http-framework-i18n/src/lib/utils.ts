ceab5a42b387dd2d1cd0e8afa7115794a9fceda5
A. Román <kyradiscord@gmail.com>
2022-08-07 14:28:16 +0200

    feat: add `applyNameLocalizedBuilder` among others (#138)

--- packages/http-framework-i18n/src/lib/utils.ts
+++ packages/http-framework-i18n/src/lib/utils.ts
@@ -4,7 +4,7 @@ import { lazy } from '@sapphire/utilities';
 import type { APIInteraction, APIPingInteraction, LocaleString, LocalizationMap } from 'discord-api-types/v10';
 import type { TFunction, TOptions, TOptionsBase } from 'i18next';
 import { getT, loadedLocales } from './registry';
-import type { BuilderWithNameAndDescription, LocalePrefixKey, TypedFT, TypedT } from './types';
+import type { LocalePrefixKey, TypedFT, TypedT } from './types';
 
 export type Interaction = Exclude<APIInteraction, APIPingInteraction>;
 
@@ -71,37 +71,76 @@ export function resolveKey(interaction: Interaction, ...args: [any, any, any?]) 
 }
 
 const getLocales = lazy(() => new Collection([...loadedLocales].map((locale) => [locale, getT(locale)])));
-
-function getDefaultT() {
+const getDefaultT = lazy(() => {
 	const defaultT = getLocales().get('en-US');
 	if (defaultT) return defaultT;
 	throw new TypeError('Could not find en-US');
-}
+});
 
-export function applyLocalizedBuilder<T extends BuilderWithNameAndDescription>(
-	builder: T,
-	...params: [root: LocalePrefixKey] | [name: TypedT, description: TypedT]
-): T {
-	const locales = getLocales();
-	const defaultT = getDefaultT();
-
-	const [localeName, localeDescription] = params.length === 1 ? [`${params[0]}Name` as TypedT, `${params[0]}Description` as TypedT] : params;
-
-	return builder
-		.setName(defaultT(localeName))
-		.setNameLocalizations(Object.fromEntries(locales.map((t, locale) => [locale, t(localeName)])))
-		.setDescription(defaultT(localeDescription))
-		.setDescriptionLocalizations(Object.fromEntries(locales.map((t, locale) => [locale, t(localeDescription)])));
-}
-
-export function createSelectMenuChoiceName<V extends NonNullObject>(nameKey: TypedT, value?: V): createSelectMenuChoiceName.Result<V> {
+/**
+ * Gets the value and the localizations from a language key.
+ * @param key The key to get the localizations from.
+ * @returns The retrieved data.
+ * @remarks This should be called **strictly** after loading the locales.
+ */
+export function getLocalizedData(key: TypedT): LocalizedData {
 	const locales = getLocales();
 	const defaultT = getDefaultT();
 
 	return {
+		value: defaultT(key),
+		localizations: Object.fromEntries(locales.map((t, locale) => [locale, t(key)]))
+	};
+}
+
+/**
+ * Applies the localized names on the builder, calling `setName` and `setNameLocalizations`.
+ * @param builder The builder to apply the localizations to.
+ * @param key The key to get the localizations from.
+ * @returns The updated builder.
+ */
+export function applyNameLocalizedBuilder<T extends BuilderWithName>(builder: T, key: TypedT) {
+	const result = getLocalizedData(key);
+	return builder.setName(result.value).setNameLocalizations(result.localizations);
+}
+
+/**
+ * Applies the localized descriptions on the builder, calling `setDescription` and `setDescriptionLocalizations`.
+ * @param builder The builder to apply the localizations to.
+ * @param key The key to get the localizations from.
+ * @returns The updated builder.
+ */
+export function applyDescriptionLocalizedBuilder<T extends BuilderWithDescription>(builder: T, key: TypedT) {
+	const result = getLocalizedData(key);
+	return builder.setDescription(result.value).setDescriptionLocalizations(result.localizations);
+}
+
+/**
+ * Applies the localized names and descriptions on the builder, calling {@link applyNameLocalizedBuilder} and
+ * {@link applyDescriptionLocalizedBuilder}.
+ * @param builder The builder to apply the localizations to.
+ * @param params The root key or the key for the name and description keys.
+ * @returns The updated builder.
+ * @remarks If only 2 parameters were passed, `name` will be defined as `${root}Name` and `description` as
+ * `${root}Description`, being `root` the second parameter in the function, after `builder`.
+ */
+export function applyLocalizedBuilder<T extends BuilderWithNameAndDescription>(
+	builder: T,
+	...params: [root: LocalePrefixKey] | [name: TypedT, description: TypedT]
+): T {
+	const [localeName, localeDescription] = params.length === 1 ? [`${params[0]}Name` as TypedT, `${params[0]}Description` as TypedT] : params;
+
+	applyNameLocalizedBuilder(builder, localeName);
+	applyDescriptionLocalizedBuilder(builder, localeDescription);
+	return builder;
+}
+
+export function createSelectMenuChoiceName<V extends NonNullObject>(key: TypedT, value?: V): createSelectMenuChoiceName.Result<V> {
+	const result = getLocalizedData(key);
+	return {
 		...value,
-		name: defaultT(nameKey),
-		name_localizations: Object.fromEntries(locales.map((t, locale) => [locale, t(nameKey)]))
+		name: result.value,
+		name_localizations: result.localizations
 	} as createSelectMenuChoiceName.Result<V>;
 }
 
@@ -111,3 +150,20 @@ export namespace createSelectMenuChoiceName {
 		name_localizations: LocalizationMap;
 	};
 }
+
+export interface LocalizedData {
+	value: string;
+	localizations: LocalizationMap;
+}
+
+export interface BuilderWithName {
+	setName(name: string): this;
+	setNameLocalizations(localizedNames: LocalizationMap | null): this;
+}
+
+export interface BuilderWithDescription {
+	setDescription(description: string): this;
+	setDescriptionLocalizations(localizedDescriptions: LocalizationMap | null): this;
+}
+
+export type BuilderWithNameAndDescription = BuilderWithName & BuilderWithDescription;
