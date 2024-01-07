73c94eee30ab5cc8ef5a7aed25eee53595d95306
Swiizyy <williams@wbussat.fr>
2024-01-07 16:46:30 +0100

    chore(influx-utilities): remove unused function

--- packages/influx-utilities/src/lib/common.ts
+++
@@ -1,9 +0,0 @@
-export function tryNumberParse(value: string | undefined) {
-	if (typeof value === 'string') {
-		const number = Number(value);
-		if (Number.isNaN(number)) throw new TypeError(`Could not parse ${value} to a number`);
-		return number;
-	}
-
-	return value;
-}
