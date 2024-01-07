bbc557744833915a766e627f9de45d943acf1684
Swiizyy <williams@wbussat.fr>
2024-01-07 11:14:12 +0100

    chore(influx-utilities): add docs to ConnectionOptions

--- packages/influx-utilities/src/lib/types.ts
+++ packages/influx-utilities/src/lib/types.ts
@@ -1,7 +1,21 @@
 import type { ClientOptions, WritePrecisionType } from '@influxdata/influxdb-client';
 
 export interface ConnectionOptions extends Partial<ClientOptions> {
-	org: string;
-	writeBucket: string;
-	writePrecision: WritePrecisionType;
+	/**
+	 * The URL to connect to.
+	 * @default process.env.INFLUX_ORG
+	 */
+	org?: string;
+
+	/**
+	 * The bucket to write to.
+	 * @default process.env.INFLUX_BUCKET
+	 */
+	writeBucket?: string;
+
+	/**
+	 * The precision of the timestamp.
+	 * @default 's'
+	 */
+	writePrecision?: WritePrecisionType;
 }
