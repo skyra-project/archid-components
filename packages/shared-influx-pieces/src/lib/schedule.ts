124550aa838df39a5b0e1900775f4655e3038b76
Swiizyy <williams@wbussat.fr>
2024-01-07 17:03:56 +0100

    refactor(shared-influx-pieces): remove timer-manager

--- packages/shared-influx-pieces/src/lib/schedule.ts
+++ packages/shared-influx-pieces/src/lib/schedule.ts
@@ -1,4 +1,3 @@
-import { TimerManager } from '@sapphire/timer-manager';
 import { container } from '@skyra/http-framework';
 import { getApproximateGuildCount } from './api.js';
 
@@ -8,13 +7,13 @@ let analyticsSyncInterval: NodeJS.Timeout | null = null;
 let resourceAnalyticsSyncInterval: NodeJS.Timeout | null = null;
 
 export function registerSchedule() {
-	analyticsSyncInterval = TimerManager.setInterval(async () => {
+	analyticsSyncInterval = setInterval(async () => {
 		const guilds = await getApproximateGuildCount();
 		if (!guilds) return;
 		return container.client.emit('analyticsSync', guilds);
 	}, Minute * 10);
 
-	resourceAnalyticsSyncInterval = TimerManager.setInterval(() => container.client.emit('resourceAnalyticsSync'), Minute);
+	resourceAnalyticsSyncInterval = setInterval(() => container.client.emit('resourceAnalyticsSync'), Minute);
 }
 
 export function getAnalyticsSyncInterval() {
