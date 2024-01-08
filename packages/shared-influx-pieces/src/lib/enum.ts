7c81fee9d5b78bbb159f460beec27689f6dcb464
Swiizyy <williams@wbussat.fr>
2024-01-08 13:32:07 +0100

    fix(shared-influx-pieces): cleanup enums

--- packages/shared-influx-pieces/src/lib/enum.ts
+++ packages/shared-influx-pieces/src/lib/enum.ts
@@ -1,13 +1,10 @@
 export enum Points {
 	ApproximateGuilds = 'approximate_guilds',
 	Commands = 'commands',
-	PerCoreCPULoad = 'per_core_cpu_load',
-	Memory = 'memory',
 	InteractionCount = 'interaction_count'
 }
 
 export enum Tags {
-	Shard = 'shard',
 	Client = 'client_id',
 	Action = 'action',
 	OriginEvent = 'origin_event'
