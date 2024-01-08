d461f99297bbec89466a44d890e0bbbea84ba8ef
Swiizyy <williams@wbussat.fr>
2024-01-08 14:00:36 +0100

    fix(shared-influx-pieces): fix export functions

--- packages/shared-influx-pieces/src/index.ts
+++ packages/shared-influx-pieces/src/index.ts
@@ -2,7 +2,7 @@ import { type BooleanString } from '@skyra/env-utilities';
 import { Client } from '@skyra/influx-utilities';
 
 export { InfluxListener } from './lib/InfluxListener.js';
-export { initializeInflux, getInteractionCount, setInteractionCount, incrementInteractionCount } from './lib/functions.js';
+export { initializeInflux, isInfluxInitialized } from './lib/functions.js';
 
 declare module '@sapphire/pieces' {
 	export interface Container {
