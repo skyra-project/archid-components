f7e5ee8a01129ffd9a086af28fab5f287b9c05a4
Swiizyy <williams@wbussat.fr>
2024-01-08 13:30:09 +0100

    fix(shared-influx-pieces): use `APIApplication` from `discord-api-types`

--- packages/shared-influx-pieces/src/lib/api.ts
+++ packages/shared-influx-pieces/src/lib/api.ts
@@ -3,7 +3,7 @@ import { container } from '@skyra/http-framework';
 import { Routes, type APIApplication } from 'discord-api-types/v10';
 
 export async function getApproximateGuildCount() {
-	const result = await Result.fromAsync(() => container.rest.get(Routes.currentApplication()) as Promise<Application>);
+	const result = await Result.fromAsync(() => container.rest.get(Routes.currentApplication()) as Promise<APIApplication>);
 
 	return result.match({
 		ok: (value) => value.approximate_guild_count ?? null,
@@ -12,14 +12,10 @@ export async function getApproximateGuildCount() {
 }
 
 export async function getClientId() {
-	const result = await Result.fromAsync(() => container.rest.get(Routes.currentApplication()) as Promise<Application>);
+	const result = await Result.fromAsync(() => container.rest.get(Routes.currentApplication()) as Promise<APIApplication>);
 
 	return result.match({
 		ok: (value) => value.id ?? null,
 		err: () => null
 	});
-}
-
-interface Application extends APIApplication {
-	approximate_guild_count?: number;
 }
