60731659d36f18309d6834f94188b162e78e4e98
Seren_Modz 21 <seren@kings-world.net>
2025-02-12 17:23:13 +0000

    feat(env-utilities): add support for @skyra/env-utilities/setup
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- packages/env-utilities/tsup.config.ts
+++ packages/env-utilities/tsup.config.ts
@@ -3,7 +3,8 @@ import type { Options } from 'tsup';
 import { createTsupConfig } from '../../scripts/tsup.config.js';
 
 const defaultOptions: Options = {
-	esbuildPlugins: [esbuildPluginVersionInjector()]
+	esbuildPlugins: [esbuildPluginVersionInjector()],
+	entry: ['src/index.ts', 'src/setup.ts']
 };
 
 export default createTsupConfig({
